16cde8c28f351c29d7ca58af7301e0fc27a819af
ibrahimptester <ibrahimtareq159753@gmail.com>
2025-09-08 01:36:12 +0600

    variables

---
+++ 03 - Variables and Constants/variables.js
@@ -0,0 +1,10 @@
+// We can define variables using var, let or const
+// var is not used in modern age
+// let is used for variables that can change 
+// const is used for variables that do not change
+
+let a = "Ibrahim"
+console.log("My name is:", a)
+
+const b = "24"
+console.log("I am ",b,"years old") 
