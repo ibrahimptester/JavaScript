bd0a04ad24ffb56039ddd614ab74ad682ae01722
ibrahimptester <ibrahimtareq159753@gmail.com>
2025-09-09 00:14:05 +0600

    Data Types in JS

---
+++ 04 - Data Types in JS/Data_types.js
@@ -0,0 +1,40 @@
+/* There are many data types in JavaScript including
+1. String
+2. Number
+3. BigInt
+4. Boolean 
+5. Undefined 
+6. Null
+*/
+
+// 1. Example of string
+
+let a = "Ibrahim";
+console.log(a)
+
+// 2. Example of Number
+let n = 10;
+console.log(n)
+
+
+// 3. Example of BigInt
+let b = BigInt(9999999999999999)
+console.log(typeof b)
+
+//4. Boolean
+let bo = (10 === '10')
+console.log(bo)
+
+// 5. undefined
+let u;
+console.log(u)
+
+//6. Null
+let nul = null;
+console.log(nul)
+
+// 7. object
+let ob = { name: "ibrahim", age: "25"
+};
+console.log(ob.name)
+console.log(ob.age)
