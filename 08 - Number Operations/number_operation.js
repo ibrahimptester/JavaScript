de23f97a733a05a93a55774b26890f3d4a74dfe3
ibrahimptester <ibrahimtareq159753@gmail.com>
2025-09-10 00:24:08 +0600

    Number Operations

---
+++ 08 - Number Operations/number_operation.js
@@ -0,0 +1,20 @@
+/* 
+basic number operations: Addition, Subtraction, Multiplication, Division
+*/
+
+// Addition
+let product1 = 20, product2 = 10;
+console.log("Total price is:", product1 + product2);
+
+// Subtraction
+console.log("The difference between products is:", product1 - product2)
+
+// Multiplication
+let width = "100px", height = "300px";
+let area = parseInt(width) * parseInt(height)
+console.log(`The total area is ${area}px`)
+
+// Division
+let totalBill = 400;
+let number_of_persons = 4;
+console.log("Every persons share is:", totalBill / number_of_persons)
