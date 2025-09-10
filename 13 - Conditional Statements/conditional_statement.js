b80fc07a31c5c9a00f7a0908fca7bea07b919cac
ibrahimptester <ibrahimtareq159753@gmail.com>
2025-09-11 01:23:45 +0600

    conditional statements

---
+++ 13 - Conditional Statements/conditional_statement.js
@@ -0,0 +1,25 @@
+/* Conditional Statements: if, else, else if  */
+
+// All in one example 
+let points = abc
+
+ // 200 - 399 bronze 
+ // 400 - 599 silver 
+ // 600 - 799 gold 
+ // 800 - 999 diamond
+
+
+if (points <= 200) {
+    console.log("You're not a league")
+} else if (points <= 399) {
+    console.log("You're on bronze league");
+} else if (points <= 599) {
+    console.log("You're on silver league");
+} else if (points <= 799) {
+    console.log("You're on gold league");
+} else if (points <= 999) {
+        console.log("You're on diamond league")
+    } else {
+        console.log('You are the ultimate champion');
+    }
+    
