622e5c34c73098a537bc3c717aae0f86abac2505
ibrahimptester <ibrahimtareq159753@gmail.com>
2025-09-11 00:48:43 +0600

    Primitive vs NonPrimitive

---
+++ 11 - Primitive and Non-Primitive Data types/primitive_vs_non_primitive.js
@@ -0,0 +1,28 @@
+/* Primitive Data Types ( these are immutable meaning they can't be changed):
+1. String
+2. Number
+3. Boolean
+4. undefine
+5. null
+6. Symbol ex. {
+let id = symbol('uid')
+}
+*/
+
+/* 
+Non primitive data types ( they are mutable meaning they can be changed) :
+1. Object 
+let object = {
+    name: "Ibrahim"
+    age: "24"
+}
+2. Array
+let array = [25, 55, 45, 65]
+
+3. Function 
+function functionName(parameter) {
+//block of code 
+console.log('Hello, I am a function!')
+}
+*/
+
